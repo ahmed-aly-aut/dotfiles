1544c379e187f4f62d9f6241e2c82e8e0225741a
ahaly <ahmed.aly@compass.at>
2025-09-11 11:51:03 +0200

    update colors

---
+++ nvim/lua/plugins/colorschema.lua
@@ -0,0 +1,5 @@
+return {
+    { "rose-pine/neovim",      name = "rose-pine" },
+    { "catppuccin/nvim",       name = "catppuccin", priority = 1000 },
+    { "folke/tokyonight.nvim", name = "tokyonight", priority = 1000, lazy = false, opts = {} },
+}
