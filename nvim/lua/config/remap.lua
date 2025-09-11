33fa0f7183ed550676b9d336b2f28cabc060d15f
ahaly <ahmed.aly@compass.at>
2025-09-11 11:52:17 +0200

    add Oil plugin

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,9 +1,8 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", ":Oil<CR>")
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
 
 vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>")
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
-
 
 vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y<CR>')
 vim.keymap.set({ "n", "v", "x" }, "<leader>w", '"+d<CR>')
